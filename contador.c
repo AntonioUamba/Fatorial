d366a051a4295e4b3d87af0dda1fbbbef7e3da7c
Antonio Paulo Umaba <131793932+AntonioUamba@users.noreply.github.com>
2023-06-20 00:02:22 -0300

    Add files via upload

---
+++ contador.c
@@ -0,0 +1,25 @@
+// inserindo as bibliotecas
+
+#include <16F628A.h>
+#fuses XT, NOMCLR, NOWDT
+#use delay(clock=4MHz)
+
+// declara;ão das variaveis 
+int8 disp[] = {
+   0b00111111, 0b00000110, 0b01011011, 0b01001111,
+   0b01100110, 0b01101101, 0b01111100, 0b00000111,
+   0b01111111, 0b01100111
+};
+
+void main () {
+
+   int numero = 0;   
+   while (true) { 
+      output_b( disp[numero] );
+      numero++;
+      if (numero > 9) {
+         numero = 0;
+      }
+      delay_ms(500);
+   }
+}
